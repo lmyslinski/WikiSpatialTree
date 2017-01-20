4959e6c0aba768da7a25590ffa7b55c34a743700
aradys <agata.radys@gmail.com>
2017-01-20 13:55:09 +0100

    add data from polish wikipedia

--- build.py
+++ build.py
@@ -5,10 +5,10 @@ from preprocessing.builder import Dataset
 # with open('matrix.pickle', 'rb') as handle:
 #     matrix = pickle.load(handle)
 
-ds = Dataset("polish", 'data/polish/simple-20120104-cattreeid.twr',
-             'data/polish/simple-20120104-titlecat.twr',
-             'data/polish/simple-20120104-catlinks.twr',
-             'data/polish/simple-20120104-pagetitle.twr', ' ')
+ds = Dataset("polish", 'data/polish/pl-20170101-cattreeid.twr',
+             'data/polish/pl-20170101-titlecat.twr',
+             'data/polish/pl-20170101-catlinks.twr',
+             'data/polish/pl-20170101-pagetitle.twr', ' ')
 
 ds2 = Dataset("simple", 'data/simple/categories', 'data/simple/simple-20120104-titlecat.twr',
               'data/simple/simple-20120104-catlinks.twr', 'data/simple/simple-20120104-pagetitle.twr', '\t')
