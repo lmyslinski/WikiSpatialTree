af0c66e637a9f23a9604f277f71455c2c78af184
aradys <agata.radys@gmail.com>
2017-05-17 23:46:42 +0200

    BOW added to nodes

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,40 +1,30 @@
 import logging, gensim
+import cPickle as pickle
+from graph_tool import Graph
 from gensim import corpora
+from scipy.linalg import norm
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 def get_children(g, vertex, edges):
     return [g.vertex(x.target()) for x in filter(lambda y: y.target() != vertex, edges)]
 
-def getAllChildArticles(g, vertex):
-    articles = g.vp.articles[vertex]
-    children = get_children(g, vertex, list(vertex.all_edges()))
-    for child in children:
-        articles.extend(getAllChildArticles(g, child))
-    return articles
-
 def bag_of_words(graph):
 
-    docLabels = []
-    documents = []
+    docs = []
 
-    # prepare labels and merge articles from vectors
     for vertex in graph.vertices():
-        docLabels.append(graph.vp.title[vertex])
+        articles_text = ""
+        for article in graph.vp.articles[vertex]:
+            articles_text = articles_text + article
 
-        articles_vec = getAllChildArticles(graph, vertex)
+        docs.append(articles_text.split())
 
-        articles = ""
-        for par in articles_vec:
-            articles = articles + par
-
-        documents.append(articles)
-
-    # create & save a dictionary
-
-    # remove common words and tokenize
+    # # create & save a dictionary
+    #
+    # # remove common words and tokenize
     # stoplist = set('for a of the and to in'.split())
-    # texts = [[word for word in document.lower().split() if word not in stoplist]
-    #          for document in documents]
+    # texts = [[word for word in document.words if word not in stoplist]
+    #          for document in docs]
     #
     # # remove words that appear only once
     # from collections import defaultdict
@@ -52,8 +42,24 @@ def bag_of_words(graph):
     dictionary = corpora.Dictionary.load('/tmp/bag_of_words.dict')
     print(dictionary)
 
+    for vertex in graph.vertices():
+        graph.vp.bow[vertex] = dictionary.doc2bow(docs[int(vertex)])
+        print vertex
+
+
+
+    # categories = ['Geography', 'Engineering', "Computer_science", "Clothing"]
+    # bows = []
+    #
     # for vertex in graph.vertices():
-        # graph.vp.bow[vertex] = dictionary.doc2bow(documents[vertex].lower().split())
+    #     if graph.vp.title[vertex] in categories:
+    #         bows.append(graph.vp.bow[vertex])
+    #
+    # print 'Geography - Engineering'
+    # print cosine_sim(bows[0],bows[1])
+    #
+    # print 'Geography - Clothing'
+    # print cosine_sim(bows[0],bows[3])
 
 
 
